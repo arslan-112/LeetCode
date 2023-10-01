b780ae91ded7fa7ff1a8e2f684070de8a8dee1af
Arslan Ijaz <76623349+arslan-112@users.noreply.github.com>
2023-10-01 14:00:57 +0500

    Time: 0 ms (100%), Space: 43.9 MB (19.92%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int [] shuffled = new int[nums.length];
-        
         for(int i=0, j = 0;i<nums.length;i++){
             if(i%2 == 0){
-                shuffled[i] = nums[j]; j++;
+                shuffled[i] = nums[j++]; 
             }else{
-                shuffled[i] = nums[n]; n++;
+                shuffled[i] = nums[n++]; 
             }
                 
         }
