8f79abc49a17605e6a5470b67099bbeadae3adc1
Arslan Ijaz <76623349+arslan-112@users.noreply.github.com>
2023-10-04 23:08:05 +0500

    Time: 0 ms (100%), Space: 43 MB (54.28%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(nums[i]<0)
                 negatives++;
-            if(nums[i] ==0)
+            else if(nums[i] ==0)
                 return 0;
         }
         if(negatives %2 == 0)
