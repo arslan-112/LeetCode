3d2ed5aa3d67ef5e488c6f0482dc1579141de313
Arslan Ijaz <76623349+arslan-112@users.noreply.github.com>
2023-10-08 01:43:01 +0500

    Time: 2 ms (79.11%), Space: 40.1 MB (60.78%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.java
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean Selfdivide(int Onum){
         int num = Onum;
-        
         boolean divides = true;
         while (num >0){
             int rem = num%10;
             if(rem == 0){
-                return false;        
+                return false;
             }
             num/=10;
-            if (Onum%rem !=0){
+            if (Onum%rem !=0){  
                 return false;
             }
             
@@ -19,8 +18,8 @@ class Solution {
     public List<Integer> selfDividingNumbers(int left, int right) {
         List<Integer> list = new ArrayList<Integer>();
         for(int i=left; i<=right;i++){
-            if(Selfdivide(i)){   //Used the function to improve run time
-                list.add(i);     //Without function runtime was 3ms
+            if(Selfdivide(i)){   //Function improves runtime by 50%
+                list.add(i);
             }
         }
         return list;
