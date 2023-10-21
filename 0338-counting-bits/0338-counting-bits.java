cfbb45dc72dd5e8530cc57920f5f88e72e7e4631
Arslan Ijaz <76623349+arslan-112@users.noreply.github.com>
2023-10-21 23:50:10 +0500

    Time: 4 ms (28.35%), Space: 46.5 MB (43.74%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,12 +1,16 @@
 class Solution {
     public int[] countBits(int n) {
         int [] ans = new int[n+1];
-        for(int i=0; i<=n; i++){
+        for(int i=1; i<=n; i++){
+            if(i%2 != 0)
+                ans[i] = ans[i-1] +1;
+            else{
             int j = i;
             while(j>0){
                 ans[i] += j%2;
                 j/= 2;
             }
+        }
         }
         return ans;
     }
