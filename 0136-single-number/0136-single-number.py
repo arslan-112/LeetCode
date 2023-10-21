b8762f1cb1c67355ba8fbfba74893cf8b768ee3c
Arslan Ijaz <76623349+arslan-112@users.noreply.github.com>
2023-10-22 00:28:28 +0500

    Time: 113 ms (95.34%), Space: 19.4 MB (5.67%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        set1 = set()
+        for i in nums:
+            if i not in set1:
+                set1.add(i)
+            elif i in set1:
+                set1.remove(i)
+        
+        return set1.pop()
+        
