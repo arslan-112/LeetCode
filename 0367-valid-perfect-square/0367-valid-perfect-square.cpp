e3b4cd3cbbb7dd8c83bdc04524a298b56cf8c082
Arslan Ijaz <76623349+arslan-112@users.noreply.github.com>
2024-03-16 18:59:55 +0500

    Time: 2 ms (56.28%), Space: 7.1 MB (52.25%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -6,7 +6,7 @@ public:
 
         while(start <= end)
         {
-            long long mid = start + (end - start) / 2;
+            long mid = start + (end - start) / 2;
 
             if(mid * mid == num)
             return true;
